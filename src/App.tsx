5ba2320a5d16ec9e871ef5adcf91108f715d57a6
Luc Martial <lucmartial@msn.com>
2025-01-22 19:05:06 +0100

    React-Basics-03-Pokedex-Afficher-des-données-dans-un-composant

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokemonCard from './components/PokemonCard';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+  );
+};
+
+export default App;
