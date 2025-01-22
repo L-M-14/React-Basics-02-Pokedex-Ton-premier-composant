e0157a506294be9d240031eceb7c7fd1a136b119
Luc Martial <lucmartial@msn.com>
2025-01-22 20:03:07 +0100

    React Basics 04 - Pokédex - Passer des props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const App: React.FC = () => {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 };
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,29 +1,21 @@
 import React from 'react';
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+interface Pokemon {
+    name: string;
+    imgSrc?: string;
+}
 
-const PokemonCard: React.FC = () => {
-  const pokemon = pokemonList[1]; 
-
-  return (
-    <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
-      <figcaption>{pokemon.name}</figcaption>
-    </figure>
-  );
+const PokemonCard: React.FC<{ pokemon: Pokemon }> = ({ pokemon }) => {
+    return (
+        <figure>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
 };
 
-export { pokemonList };
 export default PokemonCard;
